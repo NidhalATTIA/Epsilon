7759353c05c011c478778ee66607eadf92041774
houssembaazoug <44546929+houssembaazoug@users.noreply.github.com>
2019-10-28 16:38:22 +0100

    commiit
    first commit

---
+++ SingleService/src/Service/ServiceFeedback.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Service;
+
+import Entite.Feedback;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import utils.DataSource;
+
+/**
+ *
+ * @author houssembaazoug
+ */
+public class ServiceFeedback {
+    private Connection con = DataSource.getInstance().getConnection();
+    private Statement ste;
+
+    public ServiceFeedback() {
+        try {
+            ste=con.createStatement();
+        } catch (SQLException ex) {
+            System.out.println(ex);  
+        }
+    }
+
+    public void ajouterSujet_forum(Feedback s) throws SQLException{
+      String requete = "INSERT INTO `Review` (`ID`, `idClient`, `idFreelancer`, `Contenu`) "
+              + "VALUES (NULL, '"+s.getClient()+"', '"+s.getidFreelancer()+"', '"+s.getContenu()+"');";
+   ste.executeUpdate(requete);
+        System.out.println("elment insere");
+    }
+    
+    public void supprimerSujet_forum(int id)throws SQLException{
+   
+    String requete ="DELETE FROM `Review` WHERE `Review`.`ID` ="+id;
+    ste.executeUpdate(requete);
+        System.out.println("elment supprimmee");
+    }
+    public void modifierSujet_forum(Feedback s)throws SQLException{
+   
+    String requete ="UPDATE `Review` SET `Contenu` = '"+s.getContenu()+"' WHERE `Review`.`ID` ='"+s.getId()+"' ;";
+    ste.executeUpdate(requete);
+        System.out.println("elment modifier");
+    }
+    public List<Feedback> readAll() throws SQLException
+    {
+        List<Feedback> list=new ArrayList<>();
+    ResultSet res=ste.executeQuery("select * from Feedback ORDER BY date DESC");
+    Feedback per=null;
+    while (res.next()) {            
+      per=new Feedback(res.getInt(1), res.getInt(2),res.getInt(3), res.getString(4));
+      list.add(per);
+       }
+    return list;
+  } 
+    
+    public Feedback recherche(int id) throws SQLException
+    {
+       
+    ResultSet res=ste.executeQuery("select * from Review  WHERE `Review`.`ID` ='"+id+"' ;");
+    Feedback sujet=null;
+    while (res.next()) {            
+      sujet=new Feedback(res.getInt(1), res.getInt(2),res.getInt(3), res.getString(4));
+      
+       }
+    return sujet;
+  } 
+}
---
+++ SingleService/src/singleservice/SingleService.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package singleservice;
+
+import Entite.Sujet_forum;
+import Entite.Reclamation;
+import Entite.Commentaire;
+import Service.ServiceCommentaire;
+import Service.ServiceSujet_forum;
+import Service.ServiceReclamation;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class SingleService {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Sujet_forum s1=new Sujet_forum(1,1,"test", "contenu du test");
+        ServiceSujet_forum service=new ServiceSujet_forum();
+        ServiceReclamation service2= new ServiceReclamation();
+        Reclamation r1 = new Reclamation("reclamation 1","contemu de la reclamation",1,1);
+        Commentaire c1 = new Commentaire(1,1,1,"commentaire");
+        Commentaire c2 = new Commentaire(1,1,1,"commentaire modifier");
+        ServiceCommentaire service3 = new ServiceCommentaire();
+        Sujet_forum s3 = null;
+        Reclamation r3=null;
+        Commentaire c3 = null;
+         Sujet_forum s2=new Sujet_forum(5,1,"test avec la modification", "contenu du test avec la modification");
+        //Reclamation r2 = new Reclamation(11,"reclamation 1 modifiee","contemu de la reclamation modifiee",1,1);
+       /* try {
+            service.ajouterSujet_forum(s1);
+        } catch (SQLException ex) {
+            System.out.println(ex);  
+        }*/
+          try {
+            service2.ajouterReclamation(r1);
+        } catch (SQLException ex) {
+            System.out.println(ex);  
+        }/*
+         try {
+            service.supprimerSujet_forum(4);
+        } catch (SQLException ex) {
+            System.out.println(ex);  
+        }
+         */  try {
+            service2.supprimerReclamation(10);
+        } catch (SQLException ex) {
+            System.out.println(ex);  
+        }/*
+         try {
+            service.modifierSujet_forum(s2);
+        } catch (SQLException ex) {
+            System.out.println(ex);  
+        } 
+          try {
+           s3= service.recherche(16);
+        } catch (SQLException ex) {
+            System.out.println(ex);  
+        } */
+          try{
+          r3 = service2.recherche(5);
+        } catch (SQLException ex) {
+            System.out.println(ex);  
+        } 
+        
+         
+       
+        /*
+        List<Sujet_forum> list1=null;
+        
+        
+       try {
+            list1=service.readAll();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        List<Review> list2=null;
+        */
+        List<Reclamation> list2=null;
+       try {
+            list2=service2.readAll();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        System.out.println(r3);
+        System.out.println(s3);
+        //System.out.println(list1);
+        System.out.println(list2);
+       /* try {
+            service3.ajouterCommentaire(c1);
+        } catch (SQLException ex) {
+            System.out.println(ex);  
+        }
+        try {
+            service3.modifierCommentaire(c2);
+        } catch (SQLException ex) {
+            System.out.println(ex);  
+        } 
+        try {
+            service3.supprimerCommentaire(2);
+        } catch (SQLException ex) {
+            System.out.println(ex);  
+        }*/
+        List<Commentaire> list3=null;
+         try {
+            list3=service3.readAll(1);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        System.out.println(list3);
+         try{
+          c3 = service3.recherche(7);
+        } catch (SQLException ex) {
+            System.out.println(ex);  
+        } 
+          System.out.println(c3);
+    }
+    
+}
