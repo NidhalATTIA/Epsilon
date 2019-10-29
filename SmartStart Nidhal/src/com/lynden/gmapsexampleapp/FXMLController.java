2a4c37e5f61b283e56c602390e87a631caf5681b
ATTIA Nidhal <44205085+NidhalATTIA@users.noreply.github.com>
2019-10-29 17:11:30 +0100

    .

--- SmartStart Nidhal/src/com/lynden/gmapsexampleapp/FXMLController.java
+++ SmartStart Nidhal/src/com/lynden/gmapsexampleapp/FXMLController.java
@@ -15,12 +15,15 @@ import com.sun.prism.PhongMaterial.MapType;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 
 public class FXMLController implements Initializable, MapComponentInitializedListener {
@@ -38,36 +41,56 @@ public class FXMLController implements Initializable, MapComponentInitializedLis
         mapView.addMapInializedListener(this);
     }    
 
-    @Override
-    public void mapInitialized() {
-           LatLong SmartStart = new LatLong(36.899296, 10.189640);
-    
-        //Set the initial properties of the map.
-        MapOptions mapOptions = new MapOptions();
-        
-        mapOptions.center(new LatLong(36.899296, 10.189640))
-  //              .mapType(MapType.ROADMAP)
-                .overviewMapControl(false)
-                .panControl(false)
-                .rotateControl(false)
-                .scaleControl(false)
-                .streetViewControl(false)
-                .zoomControl(false)
-                .zoom(12);
-                   
-        map = mapView.createMap(mapOptions);
 
-        //Add markers to the map
-        MarkerOptions markerOptions1 = new MarkerOptions();
-        markerOptions1.position(SmartStart);
-              
-      
-    }   
+public void start(Stage stage) throws Exception {
+
+    //Create the JavaFX component and set this as a listener so we know when 
+    //the map has been initialized, at which point we can then begin manipulating it.
+    mapView = new GoogleMapView();
+    mapView.addMapInializedListener(this);
+
+    Scene scene = new Scene(mapView);
+
+    stage.setTitle("JavaFX and Google Maps");
+    stage.setScene(scene);
+    stage.show();
+}
+    
+@Override
+public void mapInitialized() {
+    //Set the initial properties of the map.
+    MapOptions mapOptions = new MapOptions();
+
+    mapOptions.center(new LatLong(36.899296, 10.189640))
+//            .mapType(MapType.ROADMAP)
+            .overviewMapControl(false)
+            .panControl(false)
+            .rotateControl(false)
+            .scaleControl(false)
+            .streetViewControl(false)
+            .zoomControl(false)
+            .zoom(12);
+
+    map = mapView.createMap(mapOptions);
+
+    //Add a marker to the map
+    MarkerOptions markerOptions = new MarkerOptions();
+
+    markerOptions.position( new LatLong(36.899296, 10.189640) )
+                .visible(Boolean.TRUE)
+                .title("SmartStart");
+
+    Marker marker = new Marker( markerOptions );
+
+    map.addMarker(marker);
+
+}
 
     @FXML
     private void ContactUs(ActionEvent event) throws IOException {
-               Parent root = FXMLLoader.load(getClass().getResource("ContactUSFXML.fxml"));
+               Parent root = FXMLLoader.load(getClass().getResource("C:\\Users\\Arzack\\Documents\\GitHub\\Epsilon\\SmartStart Nidhal\\src\\GUI\\ContactUsFXML.fxml"));
        BcontactUs.getScene().setRoot(root);
     }
     
 }
+
